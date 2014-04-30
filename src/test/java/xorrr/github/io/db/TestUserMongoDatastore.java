4d7f297382835936e0e196bdfce4a75484b2eb70
xorrr <derxor@gmail.com>
2014-04-30 19:55:09 +0200

    feat: create user pojo

--- src/test/java/xorrr/github/io/db/TestUserMongoDatastore.java
+++ src/test/java/xorrr/github/io/db/TestUserMongoDatastore.java
@@ -2,11 +2,19 @@ package xorrr.github.io.db;
 
 import org.junit.Test;
 
+import xorrr.github.io.model.User;
+
 public class TestUserMongoDatastore {
 
     @Test
     public void canCreateUserDatastore() {
         UserDatastore ds = new UserMongoDatastore();
+    }
+
+    @Test
+    public void canCreateUser() {
+        User u = new User();
+        u.setName("xorrr");
     }
 
 }
