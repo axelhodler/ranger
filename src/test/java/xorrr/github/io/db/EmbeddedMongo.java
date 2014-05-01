7a18d7e28f3b1200d4e9b62507c7a544768a8b6f
xorrr <derxor@gmail.com>
2014-05-01 15:47:10 +0200

    feat: implement EmbeddedMongo class

---
+++ src/test/java/xorrr/github/io/db/EmbeddedMongo.java
@@ -0,0 +1,27 @@
+package xorrr.github.io.db;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+
+public abstract class EmbeddedMongo {
+
+    public static MongodExecutable getEmbeddedMongoExecutable()
+            throws UnknownHostException, IOException {
+        IMongodConfig mongodConfig = new MongodConfigBuilder()
+                .version(Version.Main.PRODUCTION)
+                .net(new Net(EmbeddedMongoProperties.PORT, Network
+                        .localhostIsIPv6())).build();
+
+        MongodStarter runtime = MongodStarter.getDefaultInstance();
+
+        return runtime.prepare(mongodConfig);
+    }
+}
---
+++ src/test/java/xorrr/github/io/db/EmbeddedMongoProperties.java
@@ -0,0 +1,6 @@
+package xorrr.github.io.db;
+
+public class EmbeddedMongoProperties {
+
+    public final static int PORT = 12345;
+}
