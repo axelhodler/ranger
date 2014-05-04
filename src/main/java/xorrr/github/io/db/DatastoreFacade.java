6afd7830d60f6d9d7f0c7d49cd24e62a84b05681
xorrr <derxor@gmail.com>
2014-05-04 22:08:53 +0200

    feat: add MediaDatastore interaction to DatastoreFacade

--- src/main/java/xorrr/github/io/db/DatastoreFacade.java
+++ src/main/java/xorrr/github/io/db/DatastoreFacade.java
@@ -1,13 +1,17 @@
 package xorrr.github.io.db;
 
+import xorrr.github.io.model.Media;
+import xorrr.github.io.model.Range;
 import xorrr.github.io.model.User;
 
 public class DatastoreFacade {
 
     private UserDatastore ds;
+    private MediaDatastore mds;
 
-    public DatastoreFacade(UserDatastore ds) {
+    public DatastoreFacade(UserDatastore ds, MediaDatastore mds) {
         this.ds = ds;
+        this.mds = mds;
     }
 
     public void storeUser(User u) {
@@ -20,5 +24,17 @@ public class DatastoreFacade {
 
     public void deleteUserById(String id) {
         ds.deleteUserById(id);
+    }
+
+    public void storeMedia(Media m) {
+        mds.storeMedia(m);
+    }
+
+    public void addRangeToMedia(String id, Range r) {
+        mds.addRangeToMedia(id, r);
+    }
+
+    public void getAverageRangeFor(String id) {
+        mds.getAverageRange(id);
     }
 }
--- src/test/java/xorrr/github/io/db/TestDatastoreFacade.java
+++ src/test/java/xorrr/github/io/db/TestDatastoreFacade.java
@@ -11,20 +11,24 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import xorrr.github.io.model.Media;
+import xorrr.github.io.model.Range;
 import xorrr.github.io.model.User;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestDatastoreFacade {
 
     @Mock
-    UserDatastore ds;
+    UserDatastore userDs;
+    @Mock
+    MediaDatastore mediaDs;
 
     private DatastoreFacade facade;
     private final String ID = "1234";
 
     @Before
     public void setUp() {
-        facade = new DatastoreFacade(ds);
+        facade = new DatastoreFacade(userDs, mediaDs);
     }
 
     @Test
@@ -34,20 +38,47 @@ public class TestDatastoreFacade {
 
         facade.storeUser(u);
 
-        verify(ds, times(1)).storeUser(u);
+        verify(userDs, times(1)).storeUser(u);
     }
 
     @Test
     public void canTriggerToGetUser() {
         facade.getUserById(ID);
 
-        verify(ds, times(1)).getUserById(ID);
+        verify(userDs, times(1)).getUserById(ID);
     }
 
     @Test
     public void canTriggerToDeleteUser() throws Exception {
         facade.deleteUserById(ID);
 
-        verify(ds, times(1)).deleteUserById(ID);
+        verify(userDs, times(1)).deleteUserById(ID);
+    }
+
+    @Test
+    public void canTriggerToStoreMedia() {
+        Media m = new Media("www.url.org");
+        facade.storeMedia(m);
+
+        verify(mediaDs, times(1)).storeMedia(m);
+    }
+
+    @Test
+    public void canTriggerToAddRangeToMedia() {
+        Range r = new Range(42,43);
+        String id = "1234";
+
+        facade.addRangeToMedia(id, r);
+
+        verify(mediaDs, times(1)).addRangeToMedia(id, r);
+    }
+
+    @Test
+    public void canTriggerToGetAverageRange() {
+        String id = "1234";
+
+        facade.getAverageRangeFor(id);
+
+        verify(mediaDs, times(1)).getAverageRange(id);
     }
 }
