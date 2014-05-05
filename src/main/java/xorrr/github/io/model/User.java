6889e097fa26aa7cee0009fc0cf7891a35f48b40
xorrr <derxor@gmail.com>
2014-05-05 23:06:49 +0200

    fix: User pojo to conform with jackson

--- src/main/java/xorrr/github/io/model/User.java
+++ src/main/java/xorrr/github/io/model/User.java
@@ -1,7 +1,10 @@
 package xorrr.github.io.model;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class User {
 
+    private String objectId;
     private String name;
 
     public void setName(String name) {
@@ -10,5 +13,51 @@ public class User {
 
     public String getName() {
         return this.name;
+    }
+
+    public void setId(String id) {
+        this.objectId = id;
+    }
+
+    @JsonProperty("_id")
+    public String getId() {
+        return this.objectId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result
+                + ((objectId == null) ? 0 : objectId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (objectId == null) {
+            if (other.objectId != null)
+                return false;
+        } else if (!objectId.equals(other.objectId))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [objectId=" + objectId + ", name=" + name + "]";
     }
 }
