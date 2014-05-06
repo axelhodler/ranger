fde1afb9fcbb8a753ec1765283f606e9c4a5b6c3
xorrr <derxor@gmail.com>
2014-05-06 19:10:56 +0200

    feat: create PostMediaRoute

--- src/main/java/xorrr/github/io/rest/routes/PostMediaRoute.java
+++ src/main/java/xorrr/github/io/rest/routes/PostMediaRoute.java
@@ -1,23 +1,41 @@
 package xorrr.github.io.rest.routes;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import xorrr.github.io.db.DatastoreFacade;
+import xorrr.github.io.model.Media;
 import xorrr.github.io.rest.MappedRoutes;
+import xorrr.github.io.rest.transformation.Transformator;
 
 public class PostMediaRoute extends Route {
 
     private DatastoreFacade facade;
+    private Transformator transformator;
 
-    public PostMediaRoute(DatastoreFacade facade) {
+    public PostMediaRoute(DatastoreFacade facade, Transformator transformator) {
         super(MappedRoutes.MEDIA);
         this.facade = facade;
+        this.transformator = transformator;
     }
 
     @Override
     public Object handle(Request req, Response resp) {
-        return null;
+        try {
+            facade.storeMedia(createMediaFromRequestBody(req));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return req.body();
     }
 
+    private Media createMediaFromRequestBody(Request req)
+            throws JsonParseException, JsonMappingException, IOException {
+        return transformator.toMediaPojo(req.body());
+    }
 }
--- src/test/java/xorrr/github/io/rest/routes/TestPostMediaRoute.java
+++ src/test/java/xorrr/github/io/rest/routes/TestPostMediaRoute.java
@@ -1,26 +1,86 @@
 package xorrr.github.io.rest.routes;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import spark.Request;
+import spark.Response;
 import spark.Route;
 import xorrr.github.io.db.DatastoreFacade;
+import xorrr.github.io.model.Media;
+import xorrr.github.io.rest.transformation.Transformator;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestPostMediaRoute {
 
     @Mock
     DatastoreFacade facade;
+    @Mock
+    Transformator transformator;
+    @Mock
+    Request req;
+    @Mock
+    Response resp;
+    @Mock
+    Media media;
+
+    private final String json = "asdf";
+    private PostMediaRoute pmr;
+
+    @Before
+    public void setUp() {
+        pmr = new PostMediaRoute(facade, transformator);
+    }
 
     @Test
-    public void canCreatePostMediaRoute() {
-        PostMediaRoute pmr = new PostMediaRoute(facade);
-
+    public void postRouteSubsRoute() {
         assertTrue(Route.class.isAssignableFrom(PostMediaRoute.class));
     }
 
+    @Test
+    public void bodyIsAccessed() throws JsonParseException,
+            JsonMappingException, IOException {
+        pmr.handle(req, resp);
+
+        verify(req, times(2)).body();
+    }
+
+    @Test
+    public void bodyIsDeserializedToPojo() throws Exception {
+        when(req.body()).thenReturn(json);
+
+        pmr.handle(req, resp);
+
+        verify(transformator, times(1)).toMediaPojo(json);
+    }
+
+    @Test
+    public void mediaIsStored() throws Exception {
+        when(req.body()).thenReturn(json);
+        when(transformator.toMediaPojo(json)).thenReturn(media);
+
+        pmr.handle(req, resp);
+
+        verify(facade, times(1)).storeMedia(media);
+    }
+
+    @Test
+    public void bodyIsReturned() {
+        when(req.body()).thenReturn(json);
+
+        assertEquals(json, pmr.handle(req, resp));
+    }
 }
