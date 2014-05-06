5ad0f46db55dc0fa9d189487fc1efb7928603e64
xorrr <derxor@gmail.com>
2014-05-06 19:15:34 +0200

    feat: extend MappedRoutes

--- src/test/java/xorrr/github/io/rest/MappedRoutes.java
+++ src/test/java/xorrr/github/io/rest/MappedRoutes.java
@@ -1,5 +1,8 @@
 package xorrr.github.io.rest;
 
 public abstract class MappedRoutes {
-    public static final String MEDIA = "/media";
+    public static final String BASE = "/media";
+
+    public static final String MEDIA = BASE;
+    public static final String MEDIA_BY_ID = BASE + "/:id";
 }
