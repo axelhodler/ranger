ac1a058f90b5fe74d8f05a4bd8b31f115a93e952
xorrr <derxor@gmail.com>
2014-05-07 17:51:19 +0200

    feat: create main class for testing

---
+++ src/main/java/xorrr/github/io/Main.java
@@ -0,0 +1,25 @@
+package xorrr.github.io;
+
+import java.net.UnknownHostException;
+
+import xorrr.github.io.db.DatastoreFacade;
+import xorrr.github.io.db.MediaDatastore;
+import xorrr.github.io.db.MediaMongoDatastore;
+import xorrr.github.io.db.UserDatastore;
+import xorrr.github.io.db.UserMongoDatastore;
+import xorrr.github.io.rest.SparkFacade;
+import xorrr.github.io.rest.routes.PostMediaRoute;
+import xorrr.github.io.rest.transformation.Transformator;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        UserDatastore uds = new UserMongoDatastore();
+        MediaDatastore mds = new MediaMongoDatastore();
+        DatastoreFacade facade = new DatastoreFacade(uds, mds);
+        Transformator transformator = new Transformator();
+
+        SparkFacade rest = new SparkFacade();
+        rest.setPort(1337);
+        rest.setPostMediaRoute(new PostMediaRoute(facade, transformator));
+    }
+}
