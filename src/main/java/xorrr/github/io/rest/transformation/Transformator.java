0318c15d76a36217be2a1b90c836526452485c30
xorrr <derxor@gmail.com>
2014-05-07 15:37:05 +0200

    refactor: deal with parsing exceptions inside of Transformator

--- src/main/java/xorrr/github/io/rest/transformation/Transformator.java
+++ src/main/java/xorrr/github/io/rest/transformation/Transformator.java
@@ -2,9 +2,6 @@ package xorrr.github.io.rest.transformation;
 
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import xorrr.github.io.model.Media;
@@ -18,24 +15,63 @@ public class Transformator {
         mapper = new ObjectMapper();
     }
 
-    public String toUserJson(User u) throws JsonGenerationException,
-            JsonMappingException, IOException {
-        return mapper.writeValueAsString(u);
+    public String toUserJson(User u) {
+        String userJson = null;
+        userJson = serializeUserToJson(u, userJson);
+        return userJson;
     }
 
-    public User toUserPojo(String jsonUser) throws JsonParseException,
-            JsonMappingException, IOException {
-        return mapper.readValue(jsonUser, User.class);
+    public User toUserPojo(String jsonUser) {
+        User user = null;
+        user = deserializeJsonToUser(jsonUser, user);
+        return user;
     }
 
-    public String toMediaJson(Media m) throws JsonGenerationException,
-            JsonMappingException, IOException {
-        return mapper.writeValueAsString(m);
+    public String toMediaJson(Media m) {
+        String mediaJson = null;
+        mediaJson = serializeMediaToJson(m, mediaJson);
+        return mediaJson;
     }
 
-    public Media toMediaPojo(String jsonMedia) throws JsonParseException,
-            JsonMappingException, IOException {
-        return mapper.readValue(jsonMedia, Media.class);
+    public Media toMediaPojo(String jsonMedia) {
+        Media media = null;
+        deserializeJsonToMedia(jsonMedia, media);
+        return media;
     }
 
+    private String serializeUserToJson(User u, String userJson) {
+        try {
+            userJson = mapper.writeValueAsString(u);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userJson;
+    }
+
+    private User deserializeJsonToUser(String jsonUser, User user) {
+        try {
+            user = mapper.readValue(jsonUser, User.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    private String serializeMediaToJson(Media m, String mediaJson) {
+        try {
+            mediaJson = mapper.writeValueAsString(m);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return mediaJson;
+    }
+
+    private Media deserializeJsonToMedia(String jsonMedia, Media media) {
+        try {
+            media = mapper.readValue(jsonMedia, Media.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return media;
+    }
 }
