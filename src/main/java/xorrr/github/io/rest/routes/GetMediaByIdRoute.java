9d51a06bbad7fdcf2b2bda7820408cacfe286893
xorrr <derxor@gmail.com>
2014-05-09 01:48:33 +0200

    feat: can get avg range from media by id route

--- src/main/java/xorrr/github/io/rest/routes/GetMediaByIdRoute.java
+++ src/main/java/xorrr/github/io/rest/routes/GetMediaByIdRoute.java
@@ -5,8 +5,10 @@ import spark.Response;
 import spark.Route;
 import xorrr.github.io.db.DatastoreFacade;
 import xorrr.github.io.model.Media;
+import xorrr.github.io.model.Range;
 import xorrr.github.io.rest.MappedRoutes;
 import xorrr.github.io.rest.MappedRoutesParams;
+import xorrr.github.io.rest.QueryParams;
 import xorrr.github.io.rest.transformation.Transformator;
 
 public class GetMediaByIdRoute extends Route {
@@ -21,12 +23,36 @@ public class GetMediaByIdRoute extends Route {
     }
 
     @Override
-    public Object handle(Request request, Response response) {
+    public String handle(Request request, Response response) {
         String id = request.params(MappedRoutesParams.ID);
-        Media m = facade.getMediaById(id);
-        String json =transformator.toMediaJson(m);
+        String json = "";
 
+        if (request.queryParams().size() > 0) {
+            if (avgRangeQryParamIsTrue(request)) {
+                json = returnAvgRangeJson(id);
+            }
+        } else {
+            json = returnMediaJson(id);
+        }
         return json;
+    }
+
+    private String returnAvgRangeJson(String id) {
+        String json;
+        Range avg= facade.getAverageRangeFor(id);
+        json = transformator.toRangeJson(avg);
+        return json;
+    }
+
+    private String returnMediaJson(String id) {
+        String json;
+        Media m = facade.getMediaById(id);
+        json = transformator.toMediaJson(m);
+        return json;
+    }
+
+    private boolean avgRangeQryParamIsTrue(Request request) {
+        return request.queryParams(QueryParams.AVG_RANGE).equals("true");
     }
 
 }
