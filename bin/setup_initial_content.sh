362b02c78bcdeac8dc41e47c30355d1987200a16
xorrr <derxor@gmail.com>
2014-05-31 20:50:53 +0200

    feat: improve script to setup initial content

--- bin/setup_initial_content.sh
+++ bin/setup_initial_content.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-echo "Create user:"
+printf "Create initial users:"
 USER_ID=$(curl -X POST -d '{"login":"xorrr"}' http://localhost:1337/users)
+USER2_ID=$(curl -X POST -d '{"login":"pete"}' http://localhost:1337/users)
 
-echo "Create media:"
+printf "Create initial medias:"
 MEDIA_ID=$(curl -X POST -d '{"url":"www.foo.org"}' http://localhost:1337/media)
+MEDIA2_ID=$(curl -X POST -d '{"url":"www.bar.org"}' http://localhost:1337/media)
+MEDIA3_ID=$(curl -X POST -d '{"url":"www.baz.org"}' http://localhost:1337/media)
 
-echo "Add range to media:"
+printf "Add range to media: $MEDIA_ID\n"
+printf "Range added to media: "
 curl -X PUT -d '{"startTime":10, "endTime":20}' -H "user: $USER_ID" http://localhost:1337/media/$MEDIA_ID
+printf "\n"
+
+printf "Add range to media: $MEDIA_ID\n"
+printf "Range added to media: "
+curl -X PUT -d '{"startTime":20, "endTime":30}' -H "user: $USER2_ID" http://localhost:1337/media/$MEDIA_ID
+printf "\n"
+
+printf "Add range to media: $MEDIA2_ID\n"
+printf "Range added to media: "
+curl -X PUT -d '{"startTime":1, "endTime":5}' -H "user: $USER2_ID" http://localhost:1337/media/$MEDIA2_ID
+printf "\n"
