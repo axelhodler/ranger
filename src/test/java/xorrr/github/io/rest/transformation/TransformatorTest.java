d0849f5ef4f70d9771d3a82c261a0b5861305541
xorrr <derxor@gmail.com>
2014-06-04 15:03:58 +0200

    refactor: TransformatorTest

--- src/test/java/xorrr/github/io/rest/transformation/TransformatorTest.java
+++ src/test/java/xorrr/github/io/rest/transformation/TransformatorTest.java
@@ -34,6 +34,8 @@ public class TransformatorTest {
     private ObjectMapper mapper;
     private final String URL = "www.foo.org";
 
+    private Range r;
+
     private User createExampleUser() {
         User u = new User();
         u.setLogin("xorrr");
@@ -50,10 +52,29 @@ public class TransformatorTest {
         return m;
     }
 
+    private List<Media> createSampleMediaList() {
+        Media m1 = createExampleMedia(URL);
+        Media m2 = createExampleMedia("www.bar.org");
+        List<Media> medias = new ArrayList<>();
+        medias.add(m1);
+        medias.add(m2);
+        return medias;
+    }
+
+    private void mockMediaListTransformation() {
+        when(compliance.formatMediaList(anyString())).thenReturn("formatted");
+    }
+
+    private String transform(List<Media> medias) throws IOException,
+    JsonGenerationException, JsonMappingException {
+        return mapper.writeValueAsString(medias);
+    }
+
     @Before
     public void setUp() {
         t = new Transformator(compliance);
         mapper = new ObjectMapper();
+        r = new Range(1, 2);
     }
 
     @Test
@@ -99,8 +120,6 @@ public class TransformatorTest {
     @Test
     public void canTransformRangeToJson() throws JsonGenerationException,
             JsonMappingException, IOException {
-        Range r = new Range(1, 2);
-
         String jsonRange = mapper.writeValueAsString(r);
 
         assertEquals(r, t.toRangePojo(jsonRange));
@@ -109,25 +128,20 @@ public class TransformatorTest {
     @Test
     public void canTransformJsonToRange() throws JsonGenerationException,
             JsonMappingException, IOException {
-        Range r = new Range(1, 2);
-
         assertEquals(t.toRangeJson(r), mapper.writeValueAsString(r));
     }
 
     @Test
     public void canTransformMediaListToJson() throws JsonGenerationException,
             JsonMappingException, IOException {
-        Media m1 = createExampleMedia(URL);
-        Media m2 = createExampleMedia("www.bar.org");
-        List<Media> medias = new ArrayList<>();
-        medias.add(m1);
-        medias.add(m2);
-        when(compliance.formatMediaList(anyString())).thenReturn("formatted");
+        List<Media> medias = createSampleMediaList();
+        mockMediaListTransformation();
 
         String jsonMedias = t.toMediaJson(medias);
 
         verify(compliance, times(1)).formatMediaList(
-                mapper.writeValueAsString(medias));
+                transform(medias));
         assertEquals("formatted", jsonMedias);
     }
+
 }
