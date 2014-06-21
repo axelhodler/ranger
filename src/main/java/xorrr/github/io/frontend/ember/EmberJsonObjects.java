941469c53618e9859d51257c3c27ddbccfc8deef
xorrr <derxor@gmail.com>
2014-06-21 21:05:32 +0200

    fix: addition to previous commit

---
+++ src/main/java/xorrr/github/io/frontend/ember/EmberJsonObjects.java
@@ -0,0 +1,9 @@
+package xorrr.github.io.frontend.ember;
+
+public final class EmberJsonObjects {
+    private EmberJsonObjects() {
+    }
+
+    public static final String MEDIA = "media";
+    public static final String MEDIUM = "media";
+}
