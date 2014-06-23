747c6f48291ab8e145da860e4c87a77129379889
xorrr <derxor@gmail.com>
2014-06-23 15:34:23 +0200

    fix: initial_content script to use new model

--- bin/setup_initial_content.sh
+++ bin/setup_initial_content.sh
@@ -9,17 +9,14 @@ MEDIA_ID=$(curl -X POST -d '{"url":"www.foo.org"}' http://localhost:1337/media)
 MEDIA2_ID=$(curl -X POST -d '{"url":"www.bar.org"}' http://localhost:1337/media)
 MEDIA3_ID=$(curl -X POST -d '{"url":"www.baz.org"}' http://localhost:1337/media)
 
-printf "Add range to media: $MEDIA_ID\n"
-printf "Range added to media: "
-curl -X PUT -d '{"startTime":50, "endTime":100}' -H "user: $USER_ID" http://localhost:1337/media/$MEDIA_ID
+printf "Create range for medium: $MEDIA_ID and user: $USER_ID\n"
+curl -X POST -d '{"startTime":50, "endTime":100}' -H "Content-Type: application/json" http://localhost:1337/ranges/$MEDIA_ID?userId=$USER_ID
 printf "\n"
 
-printf "Add range to media: $MEDIA_ID\n"
-printf "Range added to media: "
-curl -X PUT -d '{"startTime":75, "endTime":144}' -H "user: $USER2_ID" http://localhost:1337/media/$MEDIA_ID
+printf "Create range for medium: $MEDIA_ID and user: $USER2_ID\n"
+curl -X POST -d '{"startTime": 100, "endTime":200}' -H "Content-Type: application/json" http://localhost:1337/ranges/$MEDIA_ID?userId=$USER2_ID
 printf "\n"
 
-printf "Add range to media: $MEDIA2_ID\n"
-printf "Range added to media: "
-curl -X PUT -d '{"startTime":20, "endTime":80}' -H "user: $USER2_ID" http://localhost:1337/media/$MEDIA2_ID
+printf "Modify range for medium: $MEDIA_ID and user: $USER_ID\n"
+curl -X PUT -d '{"startTime":20, "endTime":80}' -H "Content-Type: application/json" http://localhost:1337/ranges/$MEDIA_ID?userId=$USER_ID
 printf "\n"
