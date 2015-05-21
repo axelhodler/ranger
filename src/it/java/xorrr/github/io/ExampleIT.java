eb2186b5919f0d83095f9aff58631ae22a9df0cf
xorrr <derxor@gmail.com>
2015-05-21 23:41:05 +0200

    test: add example test that will be run as an integration test

---
+++ src/it/java/xorrr/github/io/ExampleIT.java
@@ -0,0 +1,13 @@
+package xorrr.github.io;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleIT {
+
+  @Test
+  public void trueIsTrue() {
+    assertTrue(true);
+  }
+}
